d3c11c94a8626fbf65b60d6231324cc5e5521fd7
Sara Rose <sararose623@gmail.com>
2015-09-07 23:28:04 -0400

    finished portfolio page

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 $('li').mouseover(function() { 
 	console.log( $(this) )
 
-	$(this).find( ".p1, .p2" ).show();
+	$(this).find( ".p1, .p2, .p3" ).show();
 	$(this).find( "img" ).hide();
     // $('.p1').show(); 
     // $('.img1').hide();
@@ -12,34 +12,27 @@ $('li').mouseover(function() {
 
 $('li').mouseleave(function() { 
 	console.log( $(this) )
-	$(this).find( ".p1, .p2" ).hide();
+	$(this).find( ".p1, .p2, .p3" ).hide();
 	$(this).find( "img" ).show();
     // $('.p1').hide(); 
     // $('.img1').show();
 });
 
- // $(".nav-tabs a").click(function(){
- //        $(this).tab('show');
- //    });
 
- //  $('li img').on('click',function(){
- //                var src = $(this).attr('src');
- //                var img = '<img src="' + src + '" class="img-responsive"/>';
- //                $('#myModal').modal();
- //                $('#myModal').on('shown.bs.modal', function(){
- //                    $('#myModal .modal-body').html(img);
- //                });
- //                $('#myModal').on('hidden.bs.modal', function(){
- //                    $('#myModal .modal-body').html('');
- //                });
- //           });
-document.getElementById('links').onclick = function (event) {
-    event = event || window.event;
-    var target = event.target || event.srcElement,
-        link = target.src ? target.parentNode : target,
-        options = {index: link, event: event},
-        links = this.getElementsByTagName('a');
-    blueimp.Gallery(links, options);
-};
+        $('.fancybox').fancybox({
+            
+            openEffect  : 'elastic',
+            closeEffect : 'elastic'
+        });
+
+        $(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none',
+
+
+	});
+
+      $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").fancybox();
+
 
 });
